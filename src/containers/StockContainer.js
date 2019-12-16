661e896b964b53185782778ac601dc732716ec90
kenave <kaverbookh@gmail.com>
2019-12-16 13:26:47 -0500

    Done.

--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -3,13 +3,28 @@ import Stock from '../components/Stock'
 
 class StockContainer extends Component {
 
+  // state = {
+  //   stocks: []
+  // }
+
+  createStockCards = () => {
+    let newStocks = this.props.stocks
+    return newStocks.map(stock => {
+      return <Stock {...stock} handleClick={ (stock) => this.props.addToPortfolio(stock)}/>
+    })
+  }
+
+  // componentDidMount = () => {
+  //   this.setState({stocks: this.props.stocks})
+  // }
+
   render() {
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+          {console.log('Props: ', this.props.stocks)}
+          {console.log('State: ', this.state)}
+          {this.createStockCards()}
       </div>
     );
   }
